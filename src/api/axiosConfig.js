94a86b4e0eace21a12f8b070ae7b4f2147418e93
Christian Johnson <cartian4@gmail.com>
2023-10-06 06:33:38 -0400

    Add ngrok url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL: 'https://f47d-2603-9001-8e00-2338-627-2f2b-8cd2-4a97.ngrok.io',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
