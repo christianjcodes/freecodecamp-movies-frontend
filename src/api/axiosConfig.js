dbbac4040a7808fd1d376b58b6a8c15a60a335da
Christian Johnson <cartian4@gmail.com>
2023-10-07 07:31:15 -0400

    Change baseURL and add proxy in package.json

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://f47d-2603-9001-8e00-2338-627-2f2b-8cd2-4a97.ngrok.io',
+    baseURL: 'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
