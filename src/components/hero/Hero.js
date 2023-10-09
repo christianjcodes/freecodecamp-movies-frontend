a887f193a9c294b25700e29e9044a7694f56c29c
Christian Johnson <cartian4@gmail.com>
2023-10-09 05:59:03 -0400

    Add divs for the movie carousel to Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,12 +1,32 @@
-import React from 'react'
 import './components/hero/Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 
-const Hero = () => {
+const Hero = ({movies}) => {
   return (
-    <div>
-        
+    <div className='movie-carousel-container'>
+        <Carousel>
+            {
+                movies.map((movie) => {
+                    return(
+                        <Paper>
+                            <div className='movie-card-container'>
+                                <div className='movie-card'>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt=''/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Paper>
+                    )
+                })
+            }
+        </Carousel>
     </div>
   )
 }
