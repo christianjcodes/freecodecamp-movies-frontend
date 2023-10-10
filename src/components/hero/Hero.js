8b3c779d6e3dbfa3bc094f34a507b2b6569dc51a
Christian Johnson <cartian4@gmail.com>
2023-10-10 06:32:16 -0400

    Add styling

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,17 +1,18 @@
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import uniqid from 'uniqid';
 
 const Hero = ({movies}) => {
   return (
     <div className='movie-carousel-container'>
         <Carousel>
             {
-                movies?.map((movie) => {
+                movies && movies.length > 0 ?(movies.map((movie) => {
                     return(
-                        <Paper>
+                        <Paper key={movie}>
                             <div className='movie-card-container'>
-                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                <div className='movie-card' style={{'--img': `url(${movie.backdrops?.[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={movie.poster} alt=''/>
@@ -24,7 +25,7 @@ const Hero = ({movies}) => {
                             </div>
                         </Paper>
                     )
-                })
+                })):<h4>Loading...</h4>
             }
         </Carousel>
     </div>
