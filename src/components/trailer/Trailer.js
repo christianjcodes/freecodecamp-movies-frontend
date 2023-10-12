83d81c21487508809dd520b3b6f6b549fd9259a3
Christian Johnson <cartian4@gmail.com>
2023-10-12 05:15:35 -0400

    Add imports and React component to Trailer.js

---
+++ src/components/trailer/Trailer.js
@@ -0,0 +1,15 @@
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import './Trailer.css'
+
+import React from 'react';
+
+const Trailer = () => {
+  return (
+    <div className="react-player">
+
+    </div>
+  )
+}
+
+export default Trailer
