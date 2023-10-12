c58f9bbea1d83a66b656248ecfcab9432b234c66
Christian Johnson <cartian4@gmail.com>
2023-10-12 04:59:45 -0400

    Add Buttons to Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -25,6 +25,8 @@ return (
                 <NavLink className="nav-link" to="/">Home</NavLink>
                 <NavLink className="nav-link" to="/watchlist">Home</NavLink>
                 </Nav>
+                <Button variant="outline-info" className="me-2">Login</Button>
+                <Button variant="outline-info">Register</Button>
             </Navbar.Collapse>
         </Container>
    </Navbar>
