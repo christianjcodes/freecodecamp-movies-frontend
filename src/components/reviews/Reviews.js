fedfb48d1816d477e56240f2b8dc7f6c8b499182
Christian Johnson <cartian4@gmail.com>
2023-10-12 06:00:54 -0400

    Add React component

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,2 +1,25 @@
 import {useEffect, useRef} from 'react';
+import api from '../../api/axiosConfig';
+import { useParams } from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import ReviewForm from '../reviewForm/ReviewForm';
+
+import React from 'react'
+
+const Reviews = ({getMovieData, movie, reviews, setReviews}) => {
+  
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Reviews;
 
