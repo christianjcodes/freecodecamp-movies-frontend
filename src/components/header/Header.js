9e73711016059d9be38381d0674749337180a048
Christian Johnson <cartian4@gmail.com>
2023-10-12 05:10:13 -0400

    Fix imports

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "react-bootstrap/Button";
-import { Container } from "react-bootstrap/Container";
-import { Nav } from "react-bootstrap/Nav";
-import { Navbar } from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
 
 
@@ -23,7 +23,7 @@ return (
                     navbarScroll
                 >
                 <NavLink className="nav-link" to="/">Home</NavLink>
-                <NavLink className="nav-link" to="/watchlist">Home</NavLink>
+                <NavLink className="nav-link" to="/watchlist">Watchlist</NavLink>
                 </Nav>
                 <Button variant="outline-info" className="me-2">Login</Button>
                 <Button variant="outline-info">Register</Button>
