d7b632a45b4949d9005c947825954def9e6a4c15
Christian Johnson <cartian4@gmail.com>
2023-10-11 20:28:38 -0400

    Add Navbars and Navlinks

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,10 +8,26 @@ import { NavLink } from "react-router-dom";
 
 
 const Header = () => {
-    return (
-        <div>
 
-        </div>
+return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+            <Navbar.Brand href="/" style={{"color":"gold"}}>
+                <FontAwesomeIcon icon = {faVideoSlash}/>Gold
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll"/>
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{maxHeight: "100px"}}
+                    navbarScroll
+                >
+                <NavLink className="nav-link" to="/">Home</NavLink>
+                <NavLink className="nav-link" to="/watchlist">Home</NavLink>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+   </Navbar>
     )
 }
 
